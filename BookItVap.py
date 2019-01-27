fb96cc6aa12f3f8307dbf2674b095d5df1b2e938
Alexvz2 <alexvzvz@yahoo.com>
2019-01-27 11:31:46 -0800

    Added documentation

--- BookItVap.py
+++ BookItVap.py
@@ -1,35 +1,65 @@
-# Purpose: This File containes the functions needed to generate and qunatify the
+# Purpose: This File containes the functions needed to generate and quantify the
 #          access points connections to the physical routers
 # Date: 27/01/19
 # Author: Alex Vazquez
 # bssid unqie to router
 # DA is mac address 6  octects
 # SA source adress
-# - c count after amount of seconds
-
 
 import subprocess
-import string
+import re
 
 
+# downloads and updates aircrack-ng
 def First_Time():
     subprocess.call("sudo apt-get install aircrack-ng")  # downalod aircrack if hasnt been installed
     pass
 
 
-# Device name 1 for this raspberry pi
+# Purpose: Selects and initiliazes wifi adapter to scan the traffic
+# input: device number to run monitor mode on
+# output: Treu if worked, False if error
 def Initialize(deviceNum):
-    subprocess.call("sudo airmon-ng start wlan%d" % deviceNum)
+    try:
+        subprocess.call("sudo airmon-ng start wlan%d" % deviceNum)
+    except Exception as e:
+        print("ERROR... %s" % e)
+        return
     return True
 
 
-def import_Traffic(deviceNum, tim):
-    file_ = open("ouput.txt", "w")
+# Purpose: Open .txt file and write traffic to it in determined time span
+# inputs:
+#    deviceNum: device number to run monitor mode on
+#    tim: time to execute command for
+def Import_Traffic(deviceNum, tim):
+    file_ = open("Traffic.txt", "w")
     subprocess.Popen([" timeout %i sudo tcpdump -i wlan%dmon -e" % (tim, deviceNum)], stdout=file_)
     pass
 
-def string_count(arg):
-    file_ = open("output.txt, "r")
-    MAC_Address = re.findall(('(?:[0-9a-fA-F]{1,}(?:\-|\:)){5}[0-9a-fA-F]{1,}'), addresses)
-    file_.write(MAC_Address)
-    pass
+
+# Purpose: counts number of mac adresses
+# input: size: size of max capacity of building
+# output: number of unique mac adresses
+def Mac_count(size):
+    file_ = open("Traffic.txt", "r")
+    dict = [0]*size
+    count = 0
+
+    for line in file_:
+        address = re.findall(('(?:[0-9a-fA-F]{1,}(?:\-|\:)){5}[0-9a-fA-F]{1,}'), line)
+        if not (dict.contains(address)):
+            dict.add(address)
+            count += 1
+            pass
+    return count
+
+
+# Purpose: determines the vacancy of the building
+# input:
+#   size: size of max capacity of building
+#   mac_num: unqiue mac adresses connected to AP
+# output: Vacancy percentage (out of a 100)
+def vacancy_det(size, mac_num):
+    vacancy = 100 - (size/max(mac_num, 1) * 100)  # vacancy percentage
+    return vacancy
--- OutVap.py
+++ OutVap.py
@@ -1,15 +1,52 @@
-# Purpose: Script to run eveytime website is reloaded, to illustrate Study
-#           Space vacancy
+# Purpose: Script to run every 30 mins, obtain traffic figures on the network and
+#               submit to database
+#
 # Date: 27/01/19
-from BookItVap import *
+import BookItVap as bkLib
+from pymongo import MongoClient
 import time
-import string
-import multiprocessing
+import datetime
 
 
-def upload_traffic(arg):
+client = MongoClient('mongodb://admin:admin1234@ds113815.mlab.com:13815/bookit’)
+db = client.Bookit
+collection = db.VAP
+
+
+# Inputs:
+#   t/f- True if first time running or updating pck, else false
+#   deviceNum- device number to run monitor mode one
+#   t- time span to execute and quanitify monitor mode output for
+#   size- max capacity of building
+#   location- name of location in building
+def upload_traffic(t/f, deviceNum, t, size, location):
+    if t/f:
+        bkLib.First_Time()
+        time.sleep(5)
+        pass
+    bkLib.Initialize(deviceNum, t)
+    time.sleep(5)
+    while err:
+        try:
+            bkLib.Import_Traffic(deviceNum, t)
+            time.sleep(5)
+            mac_num = bkLib.Mac_count(size)
+            building_data = {
+                "Building": "Life",
+                "Location": location,
+                "date": datetime.datetime.utcnow(),
+                "Capacity": size,
+                "Connections": mac_num,
+                "Vacancy": bkLib.vacancy_det(size, mac_num)
+                }
+            result = collection.insert_one(building_data)
+            time.sleep(1.8*10**6)  # repeat every 30 mins
+        except Exception as err:
+            raise
     pass
 
+
+# Prevent from running function without directly running script
 if __name__ == "__main__":
-    upload_traffic()
+    upload_traffic(True, 1, 100, 100000, "Life building")
     return True
